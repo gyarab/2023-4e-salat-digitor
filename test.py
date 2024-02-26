65873185247e611e14ff64bd84d8f2d5e528713b
ondrej-salat <78552892+ondrej-salat@users.noreply.github.com>
2024-02-26 10:00:25 +0100

    updated model, add test script

--- test.py
+++ test.py
@@ -1,12 +1,33 @@
 from PIL import Image
 import subprocess
+import sys
 
 
-def test_all():
-    process = subprocess.Popen(["./digitor", "digitor.json"],
+def test_all(filename):
+    process = subprocess.Popen(["./digitor", f"{filename}"],
                                stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
     count = 0
     wrongs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    print(f"# Testing on training dataset")
+    for i in range(0, 8000):
+        for j in range(0, 10):
+            image = Image.open(f'train/{j}/{j}\\{i}.png').convert("LA")
+            pixels = ""
+            for pixel in image.getdata():
+                pixels += str(pixel[1]) + " "
+            process.stdin.write(f"{pixels}\n")
+            process.stdin.flush()
+            output = process.stdout.readline().strip()
+            if output != f"{j}":
+                wrongs[j] += 1
+                count += 1
+    print(f"{count}/80000 wrong ({count/800}% correct)")
+    print(wrongs)
+    print()
+
+    count = 0
+    wrongs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    print(f"# Testing on testing dataset")
     for i in range(0, 2773):
         for j in range(0, 10):
             image = Image.open(f'test/{j}/{j}\\{i}.png').convert("LA")
@@ -17,16 +38,15 @@ def test_all():
             process.stdin.flush()
             output = process.stdout.readline().strip()
             if output != f"{j}":
-                #print(f"wrong: image: {j}/{j}\\{i}.png  output: {output}")
                 wrongs[j] += 1
                 count += 1
-    print(f"{count} wrong")
+    print(f"{count}/27730 wrong ({count/277.3}% correct)")
     print(wrongs)
 
 
-def main():
-    test_all()
+def main(filename):
+    test_all(filename)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
